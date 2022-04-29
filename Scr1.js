42b2eebce7857735190c19dfe1b8f8ac253d0ff1
Antonis Apostolidis <aapostolidis@nicon.gr>
2022-04-29 23:25:33 +0300

    First release of Hello World!

---
+++ Scr1.js
@@ -0,0 +1,20 @@
+"use strict";
+let btn = document.querySelector('button').onclick = 
+
+
+function Start(){
+ let res ='10011101'
+  this.style.backgroundColor='black';
+    console.log(" Start!");
+    let p1 = document.querySelector("#div21");
+    p1.textContent = "Done !";
+    let h1 = document.querySelector("header");
+    h1.innerHTML = "NEW HEADER !";
+    console.log(parseInt(res,2));
+   
+};
+
+//function main() {
+   
+//};
+
